5dd9f413f507f363dbd4bea48732b9b11b4f997d
unknown <khaqantabish@gmail.com>
2024-03-07 16:55:11 +0200

    Deleted unnecessary 'sample' files qt6

--- cos3711/assignment_1/working/sample2/main.cpp
+++ cos3711/assignment_1/working/sample2/main.cpp
@@ -3,13 +3,20 @@
 #include <string>
 using namespace  std;
 
+void do_cpp(){
+    string name;
+    cout << "Please enter your name: ";
+    cin >> name;
+    cout << "Hello " << name << "\n";
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
     // Do code here
+    do_cpp();
 
-    cout << "Hello World";
 
-    return a.exec();
+    // return a.exec();
 }
