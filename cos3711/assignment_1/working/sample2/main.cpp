55d43afb38e70f3549d4d257c223c53c659a525d
unknown <khaqantabish@gmail.com>
2024-03-07 16:02:48 +0200

    Trying to run 'hello world' w/ qt6 in sample/2 files

---
+++ cos3711/assignment_1/working/sample2/main.cpp
@@ -0,0 +1,15 @@
+#include <QCoreApplication>
+#include <iostream>
+#include <string>
+using namespace  std;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    // Do code here
+
+    cout << "Hello World";
+
+    return a.exec();
+}
