3fdc687eb9c2c25c32a5039ce5188a677bdf4d59
unknown <khaqantabish@gmail.com>
2024-07-04 13:36:45 +0200

    cos3711 - assign 2 - completed Qs - Q1 done, starting Q2

--- cos3711/assignment_2/finished questions folder/Q1/mainwindow.h
+++ cos3711/assignment_2/finished questions folder/Q1/mainwindow.h
@@ -1,7 +1,9 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
+#include "registrationlist.h"
 
 #include <QMainWindow>
+#include <QFileDialog>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +19,18 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
 private:
     Ui::MainWindow *ui;
+    RegistrationList newRegistrationList;
+    QList<Registration*> allRegistrations;
 };
 #endif // MAINWINDOW_H
