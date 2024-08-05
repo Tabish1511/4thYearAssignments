3493b22ad20be2109e85b2bd0cc69ba1d8ee80fa
unknown <khaqantabish@gmail.com>
2024-08-05 16:26:16 +0200

    updated git repo - x1

--- cos3711/assignment_2/working/newnew_x230_gui_working/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_newnew_x230_gui_working_FILE C:/Users/khaqa/Documents/Tabish Folder/UNISA/4thYearAssignments/cos3711/assignment_2/working/newnew_x230_gui_working/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/newnew_x230_gui_working.exe)
-set(__QT_DEPLOY_TARGET_newnew_x230_gui_working_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_newnew_x230_gui_working_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
--- cos3711/assignment_2/working/test_x1_newQT/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_test_x1_newQT_FILE C:/Users/User/Documents/Study/UNISA/MODULES/4th Year/Assignments_git/cos3711/assignment_2/working/test_x1_newQT/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/test_x1_newQT.exe)
-set(__QT_DEPLOY_TARGET_test_x1_newQT_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_test_x1_newQT_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
